ef34b0e4f3047112a78b14f38b05e5de856e16f6
Haseong Kim <dynaferkim@gmail.com>
2023-12-10 22:26:58 +0000

    Added configurator initialisation in runner builder

--- src/ServiceRunner.Runner/RunnerBuilder.cs
+++ src/ServiceRunner.Runner/RunnerBuilder.cs
@@ -1,11 +1,25 @@
-﻿using System.Runtime.CompilerServices;
+﻿using ServiceRunner.Runner.Configs;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ServiceRunner.Runner.Tests")]
 namespace ServiceRunner.Runner;
 
 public class RunnerBuilder
 {
-    public RunnerBuilder(string configPath = "config.sr")
+    public RunnerBuilder(string configPath = "./config.sr")
     {
+        if (string.IsNullOrWhiteSpace(configPath))
+        {
+            throw new ArgumentException($"Config path cannot be null or whitespace.");
+        }
+
+        if (!File.Exists(configPath))
+        {
+            Configurator.Parse(configPath);
+        }
+        else
+        {
+            Configurator.Init(configPath);
+        }
     }
 }
