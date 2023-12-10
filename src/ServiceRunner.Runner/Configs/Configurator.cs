a2c34df33d07a26d88de2988bed89801914dd4ee
Haseong Kim <dynaferkim@gmail.com>
2023-12-10 22:24:03 +0000

    Changed configurator to static class for singleton

--- src/ServiceRunner.Runner/Configs/Configurator.cs
+++ src/ServiceRunner.Runner/Configs/Configurator.cs
@@ -1,39 +1,46 @@
 using ServiceRunner.Runner.Utils;
+using System.Diagnostics;
 
 namespace ServiceRunner.Runner.Configs;
 
 /// <include file='docs/configs/configurator.xml' path='Class/Description'/>
-internal sealed class Configurator
+internal static class Configurator
 {
-    private readonly string mPath;
-    internal readonly object Config;
+    private static string mPath { get; set; } = null!;
+    internal static object Config { get; private set; } = null!;
 
-    /// <include file='docs/configs/configurator.xml' path='Class/Constructor[@Name="Init"]'/>
-    internal Configurator(string path)
+    /// <include file='docs/configs/configurator.xml' path='Class/InternalStaticMethod[@Name="Init"]'/>
+    internal static void Init(string path)
     {
+        if (mPath != null)
+        {
+            Debug.WriteLine("Configurator already initialized");
+            return;
+        }
+
         mPath = path;
         Config = new object();
     }
 
-    /// <include file='docs/configs/configurator.xml' path='Class/Constructor[@Name="WithConfig"]'/>
-    internal Configurator(string path, object config)
-    {
-        mPath = path;
-        Config = config;
-    }
-
     /// <include file='docs/configs/configurator.xml' path='Class/InternalStaticMethod[@Name="Parse"]'/>
-    internal static Configurator Parse(string path)
+    internal static void Parse(string path)
     {
+        if (mPath != null)
+        {
+            Debug.WriteLine("Configurator already initialized");
+            return;
+        }
+
         using (ByteReader reader = new ByteReader(File.ReadAllBytes(path)))
         using (ConfigurationMetadata metadata = new ConfigurationMetadata(reader))
         {
-            return new Configurator(path, metadata.Config);
+            mPath = path;
+            Config = metadata.Config;
         }
     }
 
-    /// <include file='docs/configs/configurator.xml' path='Class/InternalMethod[@Name="SaveAsFile"]'/>
-    internal void SaveAsFile()
+    /// <include file='docs/configs/configurator.xml' path='Class/InternalStaticMethod[@Name="SaveAsFile"]'/>
+    internal static void SaveAsFile()
     {
         using (ConfigurationMetadata metadata = new ConfigurationMetadata(Config))
         {
