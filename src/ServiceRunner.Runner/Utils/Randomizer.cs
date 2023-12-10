d8516933c909cd2f128682b8cf70b060b1cc7876
Haseong Kim <dynaferkim@gmail.com>
2023-12-10 20:29:57 +0000

    Created randomizer tests

--- src/ServiceRunner.Runner/Utils/Randomizer.cs
+++ src/ServiceRunner.Runner/Utils/Randomizer.cs
@@ -7,6 +7,11 @@ public static class Randomizer
     /// <include file='docs/utils/randomizer.xml' path='Class/PublicStaticMethod[@Name="GenerateRandomString"][@Type="FixedLength"]'/>
     public static string GenerateRandomString(int length)
     {
+        if (length < 0)
+        {
+            throw new ArgumentException("Length cannot be negative");
+        }
+
         return new string(Enumerable.Repeat(ALL_CHARS, length)
             .Select(s => s[Random.Shared.Next(s.Length)]).ToArray());
     }
@@ -14,6 +19,11 @@ public static class Randomizer
     /// <include file='docs/utils/randomizer.xml' path='Class/PublicStaticMethod[@Name="GenerateRandomString"][@Type="RangeLength"]'/>
     public static string GenerateRandomString(int minLength, int maxLength)
     {
+        if (minLength < 0 || maxLength < 0 || minLength > maxLength)
+        {
+            throw new ArgumentException("Length cannot be negative or min length cannot be greater than max length");
+        }
+
         int length = Random.Shared.Next(minLength, maxLength + 1);
 
         return GenerateRandomString(length);
---
+++ test/ServiceRunner.Runner.Tests/Units/Utils/RandomizerTest.cs
@@ -0,0 +1,73 @@
+using ServiceRunner.Runner.Utils;
+
+namespace ServiceRunner.Runner.Tests.Units.Utils;
+
+public class RandomizerTest
+{
+    private readonly string mAllChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    [Test]
+    public void GenerateRandomString_WithFixedLength_ShouldReturnStringOfCorrectLength()
+    {
+        int length = 10;
+        string result = Randomizer.GenerateRandomString(length);
+
+        Assert.That(result.Length, Is.EqualTo(length));
+    }
+
+    [Test]
+    public void GenerateRandomString_WithFixedLength_ShouldReturnStringWithOnlyAllowedChars()
+    {
+        int length = 10;
+        string result = Randomizer.GenerateRandomString(length);
+
+        Assert.That(result.All(mAllChars.Contains), Is.True);
+    }
+
+    [Test]
+    public void GenerateRandomString_WithRangeLength_ShouldReturnStringOfCorrectLength()
+    {
+        int minLength = 5;
+        int maxLength = 15;
+        string result = Randomizer.GenerateRandomString(minLength, maxLength);
+
+        Assert.That(result.Length, Is.GreaterThanOrEqualTo(minLength));
+        Assert.That(result.Length, Is.LessThanOrEqualTo(maxLength));
+    }
+
+    [Test]
+    public void GenerateRandomString_WithRangeLength_ShouldReturnStringWithOnlyAllowedChars()
+    {
+        int minLength = 5;
+        int maxLength = 15;
+        string result = Randomizer.GenerateRandomString(minLength, maxLength);
+
+        Assert.That(result.All(mAllChars.Contains), Is.True);
+    }
+
+    [Test]
+    public void GenerateRandomString_WithZeroLength_ShouldReturnEmptyString()
+    {
+        string result = Randomizer.GenerateRandomString(0);
+
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void GenerateRandomString_WithNegativeLength_ShouldThrowException()
+    {
+        Assert.Throws<ArgumentException>(() => Randomizer.GenerateRandomString(-1));
+    }
+
+    [Test]
+    public void GenerateRandomString_WithRangeLength_ShouldThrowException_WhenMinLengthIsGreaterThanMaxLength()
+    {
+        Assert.Throws<ArgumentException>(() => Randomizer.GenerateRandomString(10, 5));
+    }
+
+    [Test]
+    public void GenerateRandomString_WithRangeLength_ShouldThrowException_WhenMinLengthIsNegative()
+    {
+        Assert.Throws<ArgumentException>(() => Randomizer.GenerateRandomString(-1, 10));
+    }
+}
