bcee46b1a4451bfd2aefbdcbb7569d9c84bbf16d
Haseong Kim <dynaferkim@gmail.com>
2023-12-11 22:19:11 +0000

    Changed setting types to setting names

--- src/ServiceRunner.Runner/Configs/Configuration.cs
+++ src/ServiceRunner.Runner/Configs/Configuration.cs
@@ -2,6 +2,6 @@ namespace ServiceRunner.Runner.Configs;
 
 public class Configuration
 {
-    public List<string> SettingTypes { get; init; } = new List<string>();
+    public List<string> SettingNames { get; init; } = new List<string>();
     public Dictionary<string, Dictionary<string, string>> Settings { get; init; } = new Dictionary<string, Dictionary<string, string>>();
 }
