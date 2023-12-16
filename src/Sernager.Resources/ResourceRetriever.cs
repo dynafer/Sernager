57a0ad4a176dc87a9fd5adebdeece751ea2e2ba5
Haseong Kim <dynaferkim@gmail.com>
2023-12-16 23:00:58 +0000

    Created shared resource library

---
+++ src/Sernager.Resources/ResourceRetriever.cs
@@ -0,0 +1,32 @@
+﻿using System.Resources;
+
+namespace Sernager.Resources;
+
+public class ResourceRetriever : IDisposable
+{
+    private static readonly string BASE_NAMESPACE = "Sernager.Resources";
+    private readonly string mLangCode = "EN";
+
+    public ResourceRetriever(string langCode)
+    {
+        mLangCode = langCode;
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public string GetString(string resourcePath)
+    {
+        try
+        {
+            ResourceManager manager = new ResourceManager($"{BASE_NAMESPACE}.{resourcePath}", typeof(ResourceRetriever).Assembly);
+
+            return manager.GetString(mLangCode) ?? string.Empty;
+        }
+        catch (MissingManifestResourceException)
+        {
+            return string.Empty;
+        }
+    }
+}
