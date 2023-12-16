9f6d7b8e40b0ea1d5481515f7b45c021094bdfba
Haseong Kim <dynaferkim@gmail.com>
2023-12-16 14:39:10 +0000

    Created prompt key helper

---
+++ src/Sernager.Terminal/Prompts/PromptKeyHelper.cs
@@ -0,0 +1,25 @@
+namespace Sernager.Terminal.Prompts;
+
+internal static class PromptKeyHelper
+{
+    internal static bool IsCharKey(ConsoleKey key)
+    {
+        return key is >= ConsoleKey.A and <= ConsoleKey.Z
+            || key is >= ConsoleKey.D0 and <= ConsoleKey.D9
+            || key is >= ConsoleKey.NumPad0 and <= ConsoleKey.NumPad9
+            || key is ConsoleKey.Spacebar
+            || key is ConsoleKey.Oem1
+            || key is ConsoleKey.Oem2
+            || key is ConsoleKey.Oem3
+            || key is ConsoleKey.Oem4
+            || key is ConsoleKey.Oem5
+            || key is ConsoleKey.Oem6
+            || key is ConsoleKey.Oem7
+            || key is ConsoleKey.Oem8
+            || key is ConsoleKey.Oem102
+            || key is ConsoleKey.OemComma
+            || key is ConsoleKey.OemMinus
+            || key is ConsoleKey.OemPeriod
+            || key is ConsoleKey.OemPlus;
+    }
+}
