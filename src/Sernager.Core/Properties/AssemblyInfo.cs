9f2558c26bf72a6223ee7dd11745389eb7715a81
Haseong Kim <dynaferkim@gmail.com>
2023-12-22 19:10:46 +0000

    Added assembly info manually

---
+++ src/Sernager.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Sernager")]
+[assembly: AssemblyDescription("Sernager is a tool for managing envs, configs, settings, and commands.")]
+[assembly: AssemblyCompany("Sernager")]
+[assembly: AssemblyProduct("Sernager")]
+[assembly: AssemblyTrademark("Sernager")]
+[assembly: AssemblyCopyright("Sernager")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
