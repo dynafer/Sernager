1a9afb8ead1552872e751654178d1f33dc5eef5a
Haseong Kim <dynaferkim@gmail.com>
2023-12-22 18:19:52 +0000

    Created ArgAttribute

---
+++ src/Sernager.Terminal/Attributes/ArgAttribute.cs
@@ -0,0 +1,9 @@
+namespace Sernager.Terminal.Attributes;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public sealed class ArgAttribute : Attribute
+{
+    public required string Name { get; set; }
+    public string ShortName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
