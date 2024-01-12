cea00861f68b3555f789b12f9658d13544814165
Haseong Kim <dynaferkim@gmail.com>
2024-01-12 00:17:18 +0000

    Created Core.Utils.Randomizer tests

---
+++ test/Sernager.Core.Tests/Units/Utils/RandomizerFailureTests.cs
@@ -0,0 +1,42 @@
+using Sernager.Core.Tests.Fixtures;
+using Sernager.Core.Utils;
+
+namespace Sernager.Core.Tests.Units.Utils;
+
+public class RandomizerFailureTests : FailureFixture
+{
+    [Test]
+    public void GenerateRandomString_ShouldThrow_WhenPassedNegativeLength()
+    {
+        TestNoneLevel(() => Randomizer.GenerateRandomString(-1), Is.Empty);
+        TestExceptionLevel<ArgumentException>(() => Randomizer.GenerateRandomString(-1));
+    }
+
+    [Test]
+    public void GenerateRandomString_ShouldThrow_WhenPassedNegativeMinLength()
+    {
+        TestNoneLevel(() => Randomizer.GenerateRandomString(-1, 0), Is.Empty);
+        TestExceptionLevel<ArgumentException>(() => Randomizer.GenerateRandomString(-1, 0));
+    }
+
+    [Test]
+    public void GenerateRandomString_ShouldThrow_WhenPassedNegativeMaxLength()
+    {
+        TestNoneLevel(() => Randomizer.GenerateRandomString(0, -1), Is.Empty);
+        TestExceptionLevel<ArgumentException>(() => Randomizer.GenerateRandomString(0, -1));
+    }
+
+    [Test]
+    public void GenerateRandomString_ShouldThrow_WhenPassedMinLengthGreaterThanMaxLength()
+    {
+        TestNoneLevel(() => Randomizer.GenerateRandomString(1, 0), Is.Empty);
+        TestExceptionLevel<ArgumentException>(() => Randomizer.GenerateRandomString(1, 0));
+    }
+
+    [Test]
+    public void GenerateRandomString_ShouldThrow_WhenPassedNegativeMinLengthAndMaxLength()
+    {
+        TestNoneLevel(() => Randomizer.GenerateRandomString(-1, -1), Is.Empty);
+        TestExceptionLevel<ArgumentException>(() => Randomizer.GenerateRandomString(-1, -1));
+    }
+}
---
+++ test/Sernager.Core.Tests/Units/Utils/RandomizerSuccessTests.cs
@@ -0,0 +1,43 @@
+using Sernager.Core.Utils;
+
+namespace Sernager.Core.Tests.Units.Utils;
+
+public class RandomizerSuccessTests
+{
+    [Test]
+    public void GenerateRandomString_ShouldReturnEmptyString_WhenPassedZeroLength()
+    {
+        string randomString = Randomizer.GenerateRandomString(0);
+
+        Assert.That(randomString, Is.Not.Null);
+        Assert.That(randomString, Is.Empty);
+    }
+
+    [Test]
+    public void GenerateRandomString_ShouldReturnRandomString_WhenPassedValidLength()
+    {
+        string randomString = Randomizer.GenerateRandomString(10);
+
+        Assert.That(randomString, Is.Not.Null);
+        Assert.That(randomString.Length, Is.EqualTo(10));
+    }
+
+    [Test]
+    public void GenerateRandomString_ShouldReturnRandomString_WhenPassedValidMinAndMaxLengths()
+    {
+        string randomString = Randomizer.GenerateRandomString(10, 20);
+
+        Assert.That(randomString, Is.Not.Null);
+        Assert.That(randomString.Length, Is.GreaterThanOrEqualTo(10));
+        Assert.That(randomString.Length, Is.LessThanOrEqualTo(20));
+    }
+
+    [Test]
+    public void GenerateRandomString_ShouldReturnRandomString_WhenPassedSameMinAndMaxLengths()
+    {
+        string randomString = Randomizer.GenerateRandomString(10, 10);
+
+        Assert.That(randomString, Is.Not.Null);
+        Assert.That(randomString.Length, Is.EqualTo(10));
+    }
+}
