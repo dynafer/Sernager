3a021940b4e4c1aab672de820f3c50354afec7fb
Haseong Kim <dynaferkim@gmail.com>
2024-01-27 23:25:13 +0000

    Created Core.Models.GroupItemModel tests

---
+++ test/Sernager.Core.Tests/Units/Models/GroupItemModelFailureTests.cs
@@ -0,0 +1,31 @@
+using Sernager.Core.Models;
+using Sernager.Core.Tests.Fakes;
+
+namespace Sernager.Core.Tests.Units.Models;
+
+public class GroupItemModelFailureTests
+{
+    [DatapointSource]
+    private static readonly object[] _ =
+    [
+        0,
+        new int[0],
+        'a',
+        "string",
+        new FakeModel(),
+        new { Not = "Valid Model" },
+    ];
+
+    [Theory]
+    public void ItemSetter_ShouldThrow_WhenInvalidTypeGiven(object testCase)
+    {
+        Assert.Throws<InvalidCastException>(() =>
+        {
+            GroupItemModel groupItemModel = new GroupItemModel()
+            {
+                Id = Guid.NewGuid(),
+                Item = testCase
+            };
+        });
+    }
+}
